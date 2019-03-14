ab092e568b8ba1451cab21db02b241b50d6c3eb5
Nikita Chistousov <chistousov.nd@gmail.com>
2019-03-14 20:35:35 +0300

    Implemented rxjs-based search behavior

--- src/app/common/courses-controls/courses-controls.component.ts
+++ src/app/common/courses-controls/courses-controls.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil, map, distinctUntilChanged, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-courses-controls',
   templateUrl: './courses-controls.component.html',
   styleUrls: ['./courses-controls.component.css']
 })
-export class CoursesControlsComponent implements OnInit {
+export class CoursesControlsComponent implements OnInit, OnDestroy {
   @Output() searchCourses = new EventEmitter<string>();
   @Output() addCourse = new EventEmitter<void>();
 
-  public searchingCourseName = '';
+  public keyUp$ = new Subject<KeyboardEvent>();
+  public unsubscribe$ = new Subject();
 
   constructor() { }
 
   ngOnInit() {
+    this.keyUp$.pipe(
+      takeUntil(this.unsubscribe$),
+      map(event => {
+        const target = event.target as HTMLInputElement;
+        return target.value;
+      }),
+      filter(string => string.length >= 3 || string.length === 0),
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(
+      res => this.searchCourses.emit(res)
+    );
   }
 
-  onSearch(): void {
-    this.searchCourses.emit(this.searchingCourseName);
+  ngOnDestroy() {
+    this.unsubscribe$.next();
   }
 
   onAdd(): void {
     this.addCourse.emit();
   }
-
 }
