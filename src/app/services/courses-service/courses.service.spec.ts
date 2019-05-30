c2920a8adb3ca2d77c31ee13486ab5d18d1ca9a6
Chistousov, Nikita (EPAM Systems) <nikita.chistousov@astrazeneca.com>
2019-05-30 17:14:03 +0300

    Audit fix + one more courses service unit test

--- src/app/services/courses-service/courses.service.spec.ts
+++ src/app/services/courses-service/courses.service.spec.ts
@@ -8,6 +8,16 @@ describe('CourseService', () => {
   let httpTestingController: HttpTestingController;
   let service: CoursesService;
 
+  const mockCourse: ICourse = {
+    id: 0,
+    name: '',
+    length: 0,
+    authors: [],
+    date: new Date(0),
+    description: '',
+    isTopRated: false,
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -28,15 +38,7 @@ describe('CourseService', () => {
 
   describe('WHILE fetching courses data', () => {
     it('should make a GET request to /courses/id and return single course data', () => {
-      const mockResponse: ICourse = {
-        id: 0,
-        name: '',
-        length: 0,
-        authors: [],
-        date: new Date(0),
-        description: '',
-        isTopRated: false,
-      };
+      const mockResponse = Object.assign(mockCourse, {});
 
       service.getCourse(0).subscribe(res => {
         expect(res.id).toEqual(0);
@@ -53,5 +55,31 @@ describe('CourseService', () => {
 
       req.flush(mockResponse);
     });
+  });
+
+  it('should make a GET request to /courses and return courses array', () => {
+    const mockResponse = new Array<ICourse>(5);
+    mockResponse.forEach((el, i) => {
+      el = Object.assign(mockCourse, {});
+      el.id = i;
+    });
+
+    service.getCourses(5).subscribe(res => {
+      expect(res.length).toEqual(5);
+
+      const firstCourse = res[0];
+      expect(firstCourse.id).toEqual(0);
+      expect(firstCourse.name).toEqual('');
+      expect(firstCourse.length).toEqual(0);
+      expect(firstCourse.authors).toEqual([]);
+      expect(firstCourse.date).toEqual(new Date(0));
+      expect(firstCourse.description).toEqual('');
+      expect(firstCourse.isTopRated).toEqual(false);
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:3004/courses?count=5');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockResponse);
   });
 });
