a535ae5b48370bb687f0ec7d974a760c2a960244
Nikita Chistousov <chistousov.nd@gmail.com>
2019-05-31 16:11:11 +0300

    More unit test (some of them are incorrect though)

--- src/app/directives/course-highlight/course-highlight.directive.spec.ts
+++ src/app/directives/course-highlight/course-highlight.directive.spec.ts
@@ -1,11 +1,49 @@
-import { ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CourseHighlightDirective } from './course-highlight.directive';
 
-xdescribe('CourseHighlightDirective', () => {
+@Component({
+  template: `
+    <div [appCourseHighlight]="date"></div>
+  `,
+})
+class TestHostComponent {
+  public date;
+}
+
+describe('CourseHighlightDirective', () => {
+  let testHost: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let element: HTMLElement;
+
+  const MILLISECOND_IN_DAY = 1000 * 60 * 60 * 24;
+  const currentDate = new Date();
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestHostComponent, CourseHighlightDirective],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    testHost = fixture.componentInstance;
+    fixture.detectChanges();
+    element = fixture.nativeElement;
+  }));
+
   it('should create an instance', () => {
-    const el: ElementRef = new ElementRef({});
-    const directive = new CourseHighlightDirective(el);
-    expect(directive).toBeTruthy();
+    expect(testHost).toBeTruthy();
+  });
+
+  it('should make element border bold green if the course if fresh (<= 14 days)', () => {
+    const desiredDate = new Date(currentDate.getDate() - 5 * MILLISECOND_IN_DAY);
+    testHost.date = desiredDate;
+    expect(element.style.border).toBe('2px solid #98ffb2');
+  });
+
+  it('should make element border bold lightblue if the course is not started yet', () => {
+    const desiredDate = new Date(currentDate.getDate() + 5 * MILLISECOND_IN_DAY);
+    testHost.date = desiredDate;
+    expect(element.style.border).toBe('2px solid #98d0ff');
   });
 });
--- src/app/shared/course-item/course-item.component.spec.ts
+++ src/app/shared/course-item/course-item.component.spec.ts
@@ -6,6 +6,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Course } from '../../entities/course.model';
 import { AuthService } from '../../services/auth-service/auth.service';
 import { CourseItemComponent } from './course-item.component';
+import { Observable } from 'rxjs';
+import { CourseHighlightDirective } from 'src/app/directives/course-highlight/course-highlight.directive';
+import { DirectivesModule } from 'src/app/directives/directives.module';
+import { PipesModule } from 'src/app/pipes/pipes.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 class MockAuthService extends AuthService {}
 
@@ -118,7 +124,12 @@ xdescribe('CourseItemComponent AS STAND ALONE', () => {
 
 @Component({
   template: `
-    <app-course-item [data]="course" (deleting)="onDeleting($event)"></app-course-item>
+    <app-course-item [data]="course"
+                     (deleteCourse)="onDeleting($event)"
+                     (editCourse)="onEditing($event)"
+                     (viewCourse)="onViewing($event)"
+    >
+    </app-course-item>
   `,
 })
 class TestHostComponent {
@@ -131,10 +142,19 @@ class TestHostComponent {
     isTopRated: false,
     authors: [],
   };
-  public onDeleting(id: number) { console.log(`Deleting course: ${id}`); }
+  public isAdmin: Observable<boolean>;
+  public onDeleting(id: number) {
+    console.log(`Deleting course: ${id}`);
+  }
+  public onViewing(id: number) {
+    console.log(`Viewing course: ${id}`);
+  }
+  public onEditing(id: number) {
+    console.log(`Editing course: ${id}`);
+  }
 }
 
-xdescribe('CourseItemComponent AS TEST HOST COMPONENT', () => {
+describe('CourseItemComponent AS TEST HOST COMPONENT', () => {
   let testHost: TestHostComponent;
   let fixture: ComponentFixture<TestHostComponent>;
 
@@ -145,8 +165,12 @@ xdescribe('CourseItemComponent AS TEST HOST COMPONENT', () => {
         TestHostComponent,
       ],
       imports: [
+        HttpClientTestingModule,
+        DirectivesModule,
+        PipesModule,
         FontAwesomeModule,
       ],
+      providers: [provideMockStore({})],
     })
     .compileComponents();
   }));
@@ -165,23 +189,45 @@ xdescribe('CourseItemComponent AS TEST HOST COMPONENT', () => {
     const nativeElement: HTMLElement = fixture.nativeElement;
 
     const title: HTMLElement = nativeElement.querySelector('.course-item__title');
-    expect(title.textContent).toBe('Course #1');
+    expect(title.innerText).toBe('COURSE #1');
 
     const description: HTMLElement = nativeElement.querySelector('.course-item__description');
-    expect(description.textContent).toBe(' Description ');
+    expect(description.innerText).toBe('Description');
 
     const duration: HTMLElement = nativeElement.querySelector('.course-item__duration');
-    expect(duration.textContent).toBe('60 min');
+    expect(duration.innerText).toBe('1h');
 
     const created: HTMLElement = nativeElement.querySelector('.course-item__created');
-    expect(created.textContent).toBe(new Date(0).toLocaleDateString());
+    expect(created.innerText).toBe('1/1/70');
   });
 
-  it('should raise deleteing course event on click', () => {
-    console.log = jasmine.createSpy('log');
+  it('should have special class and star sign if course is topRated', () => {
+    testHost.course.isTopRated = true;
+    fixture.detectChanges();
+    const courseElement: HTMLElement = fixture.nativeElement.querySelectorAll('.course-item')[0];
+    expect(courseElement.classList.contains('course-item__top-rated')).toBeTruthy();
+    const star: HTMLElement = fixture.nativeElement.querySelectorAll('.course-item__top-rated-icon')[0];
+    expect(star).toBeTruthy();
+  });
 
+  it('should raise deleteing course event on delete button click', () => {
+    spyOn(console, 'log');
     const deleteButton: HTMLElement = fixture.nativeElement.querySelectorAll('.course-item__button')[1];
     deleteButton.click();
     expect(console.log).toHaveBeenCalledWith('Deleting course: 0');
+  });
+
+  it('should raise editing course event on edit button click', () => {
+    spyOn(console, 'log');
+    const editButton: HTMLElement = fixture.nativeElement.querySelectorAll('.course-item__button')[0];
+    editButton.click();
+    expect(console.log).toHaveBeenCalledWith('Editing course: 0');
+  });
+
+  it('should raise viewing course event on course element click', () => {
+    spyOn(console, 'log');
+    const courseElement: HTMLElement = fixture.nativeElement.querySelectorAll('.course-item')[0];
+    courseElement.click();
+    expect(console.log).toHaveBeenCalledWith('Viewing course: 0');
   });
 });
